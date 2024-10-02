73312190da3647638bd3cd295c1a8a2d424a5a7a
Parv Sharma <parv@Parvs-MacBook-Air.local>
2024-10-02 11:41:26 +0530

    backend init

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// API routes
+app.get('/', (req, res) => {
+  res.send('API Working!');
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
